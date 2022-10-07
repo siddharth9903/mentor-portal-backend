b3bc38e2071e03237fc5a93c653efc50329e4f9c
Siddharth Patel <opensiddhu993@gmail.com>
2022-10-07 12:59:26 +0530

    direct mongo connected

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const MONGO_URI="mongodb+srv://YashParsana:YashParsana2003@cluster0.0v4ejyu.mongodb.net/?retryWrites=true&w=majority"
+
 const connectDb = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log("Db connected...");
